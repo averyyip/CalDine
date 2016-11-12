d4c4250da36bc4b5e6031d96cf0a4ffc2874aa00
averyyip <averyyip@ymail.com>
2016-11-12 15:21:00 -0800

    'send_sms'

--- send_sms.py
+++ send_sms.py
@@ -38,4 +38,4 @@ def inbound_sms():
 
 if __name__ == "__main__":
     port = os.environ.get('PORT', 5000)
-    app.run(debug=True, port = int(port))
+    app.run(debug=True, host = '0.0.0.0', port = int(port))
