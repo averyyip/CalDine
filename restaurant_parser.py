92b1d935e5b4f6632267a5fd2a62df878b35ff7c
averyyip <averyyip@ymail.com>
2016-11-12 14:59:00 -0800

    t s

---
+++ restaurant_parser.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+def menu():
+	caldining = BeautifulSoup(urllib.request.urlopen('http://caldining.berkeley.edu/menus/all-locations-d1'), "html.parser")
+	restaurants = caldining.find_all("div", class_="meal_items")
+
+	result = {"Cafe 3": {"Brunch": [], "Dinner": []}, 
+	"Clark Kerr Campus": {"Breakfast":[], "Lunch":[],"Brunch": [], "Dinner": []}, 
+	"Crossroads": {"Breakfast":[], "Lunch":[], "Brunch": [], "Dinner": []}, 
+	"Foothill": {"Breakfast":[], "Lunch":[],"Brunch": [], "Dinner": []}}
+	place = ""
+	time = ""
+
+	for restaurant in restaurants:
+		for element in restaurant:
+			if "bs4.element.NavigableString" not in str(type(element)):
+				if (element.h3):
+					for e in element:
+						if "bs4.element.NavigableString" not in str(type(e)):
+							if (e['class'][0] == "location_period"):
+								time = e.get_text()
+							else:
+								result[place][time].append(e.get_text())
+				elif (element.a):
+					result[place][time].append(element.get_text())
+				else:
+					if (element['class'][0] == "location_name"):
+						place = element.get_text()
+					elif (element['class'][0] == "location_period"):
+						time = element.get_text()
+
+	result['cafe3'] = result.pop('Cafe 3')
+	result['foothill'] = result.pop('Foothill')
+	result['clarkkerr'] = result.pop('Clark Kerr Campus')
+	result['crossroads'] = result.pop('Crossroads')
+	return result
+
+def filtered_menu():
+	filtered_dict = {'cafe3':'', 'crossroads':'', 'foothill':'', 'clarkkerr':''}
+	results = menu()
+	for restaurant in results.keys():
+		for time in results[restaurant].keys():
+			if results[restaurant][time] != []:
+				filtered_dict[restaurant] += time + '\n' + ', '.join(results[restaurant][time]) + '\n\n'
+	return filtered_dict
+
+
+
+def info():
+	items = {'cafe3': '\nHours: Mon-Sun (10AM-2PM, 5PM-9PM) \nLocation: 2400 Durant Ave.',
+	'clarkkerr': '\nHours: Mon-Fri (7-9:30AM, 5:30PM-8PM), Sat-Sun (10am-2pm, 5:30pm-8pm) \nLocation: 2610 Warring St.',
+	'crossroads': '\nHours: Mon-Fri (7-10AM, 11AM-2PM, 5PM-9PM), Sat-Sun (10am-3pm, 5pm-9pm) \nLocation: 2415 Bowditch St.',
+	'foothill': '\nHours: Mon-Fri (7-9AM, 10:30AM-2PM, 5PM-8PM), Sat-Sun (10:30am-2pm, 5pm-8pm) \nLocation: 2700 Hearst Ave.',
+	'bearwalk': '\nHours: Dusk-3AM \nPhone: 510-642-9255 \nSite: http://bearwalk.berkeley.edu/',
+	'calcentral': '\nHours: Mon-Fri (9AM-4PM) \nLocation: Sproul Hall \nPhone: 510-664-9181 \nSite: https://calcentral.berkeley.edu/',
+	'gbc': '\nHours: Mon-Th (7:30AM-8PM), Fr (7:30AM-6PM) \nLocation: Upper Sproul by Sather Gate',
+	'browns': '\nHours: Mon-Th (7:30AM-6PM), Fri (7:30AM-6PM) \nLocation: Genetics & Plant Biology Rooftop',
+	'qualcomm': '\nHours: Mon-Th (7:30AM-6PM), Fri (7:30AM-4PM) \nLocation: Sutardja Dai Hall',
+	'ucpd': ' \nPhone: 510-642-3333 \nLocation: Sproul Hall \nSite: http://ucpd.berkeley.edu/'}
+	return items
+
---
+++ send_sms.py
@@ -0,0 +1,40 @@
+from flask import Flask, Response, request
+from twilio import twiml
+from restaurant_parser import info, filtered_menu
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def check_app():
+    # returns a simple string stating the app is working
+    return Response("Cal SMS App"), 200
+
+
+@app.route("/twilio", methods=["POST"])
+def inbound_sms():
+    response = twiml.Response()
+    # we get the SMS message from the request. we could also get the
+    # "To" and the "From" phone number as well
+    inbound_message = request.form.get("Body").lower().replace(' ', '')
+    # we can now use the incoming message text in our Python application
+    outbound_message = info()
+    if 'info' in inbound_message:
+        inbound_message = inbound_message.replace('info','')
+    else:
+        outbound_message.update(filtered_menu())
+    if inbound_message in outbound_message.keys():
+        response.message(inbound_message.upper() + '\n' + outbound_message[inbound_message] + '\n')
+    else:
+        commands = ''
+        dc_commands = '\ncrossroads info \nfoothill info \ncafe3 info \nclarkkerr info'
+        for key in outbound_message.keys():
+            commands += '\n' + key
+        response.message('Invalid command.\nTry :' + commands + dc_commands) #all the available options
+    # we return back the mimetype because Twilio needs an XML response
+    return Response(str(response), mimetype="application/xml"), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
