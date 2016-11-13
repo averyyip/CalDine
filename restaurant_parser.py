862dc40f3db2e5f1591d114b642a49d4ccc50e2b
averyyip <averyyip@ymail.com>
2016-11-13 09:36:38 -0800

    'soonFinalDraft'

--- restaurant_parser.py
+++ restaurant_parser.py
@@ -48,15 +48,15 @@ def filtered_menu():
 
 
 def info():
-	items = {'cafe3': '\nHours: Mon-Sun (10AM-2PM, 5PM-9PM) \nLocation: 2400 Durant Ave.',
-	'clarkkerr': '\nHours: Mon-Fri (7-9:30AM, 5:30PM-8PM), Sat-Sun (10am-2pm, 5:30pm-8pm) \nLocation: 2610 Warring St.',
-	'crossroads': '\nHours: Mon-Fri (7-10AM, 11AM-2PM, 5PM-9PM), Sat-Sun (10am-3pm, 5pm-9pm) \nLocation: 2415 Bowditch St.',
-	'foothill': '\nHours: Mon-Fri (7-9AM, 10:30AM-2PM, 5PM-8PM), Sat-Sun (10:30am-2pm, 5pm-8pm) \nLocation: 2700 Hearst Ave.',
-	'bearwalk': '\nHours: Dusk-3AM \nPhone: 510-642-9255 \nSite: http://bearwalk.berkeley.edu/',
-	'calcentral': '\nHours: Mon-Fri (9AM-4PM) \nLocation: Sproul Hall \nPhone: 510-664-9181 \nSite: https://calcentral.berkeley.edu/',
-	'gbc': '\nHours: Mon-Th (7:30AM-8PM), Fr (7:30AM-6PM) \nLocation: Upper Sproul by Sather Gate',
-	'browns': '\nHours: Mon-Th (7:30AM-6PM), Fri (7:30AM-6PM) \nLocation: Genetics & Plant Biology Rooftop',
-	'qualcomm': '\nHours: Mon-Th (7:30AM-6PM), Fri (7:30AM-4PM) \nLocation: Sutardja Dai Hall',
-	'ucpd': ' \nPhone: 510-642-3333 \nLocation: Sproul Hall \nSite: http://ucpd.berkeley.edu/'}
+	items = {'cafe3': 'Hours: Mon-Sun (10AM-2PM, 5PM-9PM) \nLocation: 2400 Durant Ave.',
+	'clarkkerr': 'Hours: Mon-Fri (7-9:30AM, 5:30PM-8PM), Sat-Sun (10am-2pm, 5:30pm-8pm) \nLocation: 2610 Warring St.',
+	'crossroads': 'Hours: Mon-Fri (7-10AM, 11AM-2PM, 5PM-9PM), Sat-Sun (10am-3pm, 5pm-9pm) \nLocation: 2415 Bowditch St.',
+	'foothill': 'Hours: Mon-Fri (7-9AM, 10:30AM-2PM, 5PM-8PM), Sat-Sun (10:30am-2pm, 5pm-8pm) \nLocation: 2700 Hearst Ave.',
+	'bearwalk': 'Hours: Dusk-3AM \nPhone: 510-642-9255 \nSite: http://bearwalk.berkeley.edu/',
+	'calcentral': 'Hours: Mon-Fri (9AM-4PM) \nLocation: Sproul Hall \nPhone: 510-664-9181 \nSite: https://calcentral.berkeley.edu/',
+	'gbc': 'Hours: Mon-Th (7:30AM-8PM), Fr (7:30AM-6PM) \nLocation: Upper Sproul by Sather Gate',
+	'browns': 'Hours: Mon-Th (7:30AM-6PM), Fri (7:30AM-6PM) \nLocation: Genetics & Plant Biology Rooftop',
+	'qualcomm': 'Hours: Mon-Th (7:30AM-6PM), Fri (7:30AM-4PM) \nLocation: Sutardja Dai Hall',
+	'ucpd': 'Phone: 510-642-3333 \nLocation: Sproul Hall \nSite: http://ucpd.berkeley.edu/'}
 	return items
 
